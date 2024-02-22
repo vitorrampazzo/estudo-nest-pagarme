940741096e573f39d446aed2b287e9a8e1412ba0
vitorrampazzo <vitor.rampazzo@gmail.com>
2024-02-21 23:32:59 -0300

    infra: MongoDB && ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://localhost/pagarme'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
